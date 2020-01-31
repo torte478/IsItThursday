725deb5006e38afea59d6b9c4a03171b20a9e23a
torte478 <torte478@gmail.com>
2020-01-31 11:27:25 +0300

    feature (test): add getting of weekday

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print ("Hello, python!")
+import datetime;
+
+today = datetime.date.today()
+weekday = today.weekday()
+if weekday == 3:
+    print ("It's Thursday!")
+else:
+    print ("It's not thursday...")
