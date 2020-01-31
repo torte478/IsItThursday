d8c8af1a9c99eea1302d84ecfbe11cb4bc971bf1
torte478 <torte478@gmail.com>
2020-01-31 21:59:31 +0300

    feature (json_config): add day/month/year conditions

--- json_config.py
+++ json_config.py
@@ -1,8 +1,49 @@
+import datetime
 import json
 
-weekdays = {'monday': 0, 'tuesday': 1, 'wendesday': 2, 'thursday': 3, 'friday': 4, 'saturday': 5, 'sunday': 6}
-with open('config.json') as json_file:
-    data = json.load(json_file)
-    weekday = data['value']
-    index = weekdays[weekday]
-    print(index)
+def read_config(name):
+    with open(name) as file:
+        data = json.load(file)
+        return (data['filter'], data['value'])
+
+def get_condition(name):
+    switch = {
+        'weekday': match_weekday,
+        'day': match_day,
+        'month': match_month,
+        'year': match_year
+        }
+    return switch.get(name)
+
+def match_weekday(date, expected):
+    weekdays = {
+        'monday': 0,
+        'tuesday': 1,
+        'wendesday': 2,
+        'thursday': 3,
+        'friday': 4,
+        'saturday': 5,
+        'sunday': 6
+        }
+    match = date.weekday() == weekdays[expected]
+    return match
+
+def match_day(date, expected):
+    return date.day == int(expected)
+
+def match_month(date, expected):
+    return date.month == int(expected)
+
+def match_year(date, expected):
+    return date.year == int(expected)
+
+
+
+
+config = read_config('config.json')
+
+today = datetime.date.today()
+condition = get_condition(config[0])
+result = condition(today, config[1])
+
+print(result)
