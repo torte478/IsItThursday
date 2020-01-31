be810ca614777423768333dd59817f1be60b58f0
torte478 <torte478@gmail.com>
2020-01-31 17:27:24 +0300

    feature (json_config): add base for condition configuring

--- json_config.py
+++ json_config.py
@@ -3,6 +3,6 @@ import json
 weekdays = {'monday': 0, 'tuesday': 1, 'wendesday': 2, 'thursday': 3, 'friday': 4, 'saturday': 5, 'sunday': 6}
 with open('config.json') as json_file:
     data = json.load(json_file)
-    weekday = data['weekday']
+    weekday = data['value']
     index = weekdays[weekday]
     print(index)
