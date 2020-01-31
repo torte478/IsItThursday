feaa3b7ed594cf20c2c9b861dedeb567d2f2d22a
torte478 <torte478@gmail.com>
2020-02-01 00:25:53 +0300

    feature (json_config): add switch-case-otherwise logic

--- json_config.py
+++ json_config.py
@@ -47,23 +47,30 @@ def match_year(expected):
 def build_expression(expression):
     if 'operator' in expression:
         if (expression['operator'] == 'not'):
-            return not(build_expression(expression['argument']))
+            expr = build_expression(expression['argument'])
+            return lambda x: not(expr(x))
         else:
             first = build_expression(expression['first'])
             second = build_expression(expression['second'])
             return (lambda x: first(x) and second(x)) if expression['operator'] == 'and' else (lambda x: first(x) or second(x))
     else:
         return get_match(expression['filter'], expression['value'])
+
+def run_cases(cases, value):
+    for case in cases['case']:
+        expression = build_expression(case['expression'])
+        match = expression(value)
+        if match: return case['result']
+
+    return cases['otherwise']    
 #            
 #
 #
 #
 #'''
 config = read_config('config.json')
-condition = build_expression(config)
 today = datetime.date.today()
-
-result = condition(today)
+result = run_cases(config, today)
 
 print(result)
 #'''
