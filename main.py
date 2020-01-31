e78f557d69dd3293505e7f04483c41107d7cd289
torte478 <torte478@gmail.com>
2020-01-31 14:31:44 +0300

    feature (main): combine base logic

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-'''
 import datetime;
-
-today = datetime.date.today()
-weekday = today.weekday()
-if weekday == 3:
-    print ("It's Thursday!")
-else:
-    print ("It's not thursday...")
-'''
-
 import os
 import ctypes
 from ctypes import wintypes
 
-SPI_SETDESKWALLPAPER  = 0x0014
-SPIF_UPDATEINIFILE    = 0x0001
-SPIF_SENDWININICHANGE = 0x0002
 
-user32 = ctypes.WinDLL('user32')
-SystemParametersInfo = user32.SystemParametersInfoW
-SystemParametersInfo.argtypes = ctypes.c_uint,ctypes.c_uint, \
-                                ctypes.c_void_p,ctypes.c_uint
-SystemParametersInfo.restype = wintypes.BOOL
-path = os.path.abspath("image1.jpg")
-res = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
-print(res)
+def get_is_thursday_value():
+    THURSDAY_INDEX = 3
+    weekday = datetime.date.today().weekday();
+    return weekday == THURSDAY_INDEX
+
+
+def get_image_path( thursday ):
+    return "thursday.jpg" if thursday else "other.jpg"
+
+
+def set_wallpaper( fileName ):
+    SPI_SETDESKWALLPAPER  = 0x0014
+    SPIF_UPDATEINIFILE    = 0x0001
+    SPIF_SENDWININICHANGE = 0x0002
+
+    user32 = ctypes.WinDLL('user32')
+    SystemParametersInfo = user32.SystemParametersInfoW
+    SystemParametersInfo.argtypes = ctypes.c_uint,ctypes.c_uint, \
+                                    ctypes.c_void_p,ctypes.c_uint
+    SystemParametersInfo.restype = wintypes.BOOL
+    path = os.path.abspath(fileName)
+    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
+
+
+    
+
+thursday = get_is_thursday_value()
+image = get_image_path(thursday)
+set_wallpaper(image)
