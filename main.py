f532c6c0e9603f074f104292e16f98730d946d0a
torte478 <torte478@gmail.com>
2020-01-31 14:14:41 +0300

    feature (main): add set wallpaper logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 import datetime;
 
 today = datetime.date.today()
@@ -6,3 +7,21 @@ if weekday == 3:
     print ("It's Thursday!")
 else:
     print ("It's not thursday...")
+'''
+
+import os
+import ctypes
+from ctypes import wintypes
+
+SPI_SETDESKWALLPAPER  = 0x0014
+SPIF_UPDATEINIFILE    = 0x0001
+SPIF_SENDWININICHANGE = 0x0002
+
+user32 = ctypes.WinDLL('user32')
+SystemParametersInfo = user32.SystemParametersInfoW
+SystemParametersInfo.argtypes = ctypes.c_uint,ctypes.c_uint, \
+                                ctypes.c_void_p,ctypes.c_uint
+SystemParametersInfo.restype = wintypes.BOOL
+path = os.path.abspath("image1.jpg")
+res = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
+print(res)
