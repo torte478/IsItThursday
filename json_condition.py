831435d8a2a26c478cc878eac73b0f673936b2a6
torte478 <torte478@gmail.com>
2020-02-04 22:43:54 +0300

    refactor(Json_Condition): replace constructors to dependencies

--- json_condition.py
+++ json_condition.py
@@ -9,22 +9,22 @@ from images.file_index import *
 from images.random import *
 from datetime_filters import *
 
-#TODO : remove constructors
 class Json_Condition:
-	def __init__(self, root):
+	def __init__(self, root, tree):
+		self.tree = tree
 		self.root = root
 
 	def condition(self):
 		cases = self.__build_cases(self.root['cases'])
 		otherwise = self.__build_images(self.root['otherwise'])
-		return Condition(cases, otherwise)
+		return self.tree.an_expressions().a_condition(cases, otherwise)
 
 	def __build_cases(self, cases):
 		result = []
 		for case in cases:
 			expression = self.__build_expression(case['expression'])
 			images = self.__build_images(case['result'])
-			result.append(Case(expression, images))
+			result.append(self.tree.an_expressions().a_case(expression, images))
 		return result
 
 	def __build_expression(self, expression):
@@ -34,30 +34,80 @@ class Json_Condition:
 			return self.__build_token(expression['filter'], expression['value'])
 
 	def __build_token(self, name, expected):
-		factory = Date_Filters()
 		if name == 'weekday':
-			return factory.weekday(expected)
+			return self.tree.a_filters().weekday(expected)
 		else:
 			if name == 'day':
-				return factory.day(expected)
+				return self.tree.a_filters().day(expected)
 			else:
 				if name == 'month':
-					return factory.month(expected)
+					return self.tree.a_filters().month(expected)
 				else:
-					return factory.year(expected)
+					return self.tree.a_filters().year(expected)
 
 	def __build_complicate_expression(self, expression):
 		if expression['operator'] == 'not':
 			argument = self.__build_expression(expression['argument'])
-			return Not_Expression(argument)
+			return self.tree.an_expressions().a_not_ex(argument)
 		else:
 			first = self.__build_expression(expression['first'])
 			second = self.__build_expression(expression['second'])
-			return And_Expression(first, second) \
+			return self.tree.an_expressions().an_and_ex(first, second) \
 					if expression['operator'] == 'and' \
-					else Or_Expression(first, second)
+					else self.tree.an_expressions().an_or_ex(first, second)
 
 	def __build_images(self, images):
-		return Random(images['values']) \
+		return self.tree.a_collections().a_random(images['values']) \
 					if images['type'] == 'random' \
-					else Enumerable(images['values'], File_Index('data.iit'))
+					else self.tree.a_collections().an_enumerable(images['values'])
+
+
+
+class Tree:
+	def __init__(self, expressions, filters, collections):
+		self.expressions = expressions
+		self.filters = filters
+		self.collections = collections
+
+	def an_expressions(self):
+		return self.expressions
+
+	def a_filters(self):
+		return self.filters
+
+	def a_collections(self):
+		return self.collections
+
+class Expressions:
+	def __init__(self, case, condition, or_ex, and_ex, not_ex):
+		self.case = case
+		self.condition = condition
+		self.or_ex = or_ex
+		self.and_ex = and_ex
+		self.not_ex = not_ex
+
+	def a_case(self, expressions, result):
+		return self.case(expressions, result)
+
+	def a_condition(self, cases, otherwise):
+		return self.condition(cases, otherwise)
+
+	def an_or_ex(self, first, second):
+		return self.or_ex(first, second)
+
+	def an_and_ex(self, first, second):
+		return self.and_ex(first, second)
+
+	def a_not_ex(self, arg):
+		return self.not_ex(arg)
+
+class Collections:
+	def __init__(self, random, enumerable):
+		self.random = random
+		self.enumerable = enumerable
+
+	def a_random(self, values):
+		return self.random(values)
+
+	def an_enumerable(self, values):
+		return self.enumerable(values)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import datetime
 import json
 
-from json_condition import Json_Condition
+from json_condition import *
 from text_file import Text_File
 from windows_api import Windows_Wallpaper
 
@@ -10,6 +10,20 @@ condition = Json_Condition( \
 				json.loads( \
 					Text_File('config.json') \
 					.text() \
+					), \
+				Tree( \
+					Expressions( \
+						lambda expressions, result: Case(expressions, result), \
+						lambda cases, otherwise: Condition(cases, otherwise), \
+						lambda first, second: Or_Expression(first, second), \
+						lambda first, second: And_Expression(first, second), \
+						lambda arg: Not_Expression(arg) \
+						), \
+					Date_Filters(), \
+					Collections( \
+						lambda values: Random(values),
+						lambda values: Enumerable(values, File_Index('data.iit')) \
+						) \
 					) \
 				) \
 			.condition()
